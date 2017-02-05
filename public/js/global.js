2fbf2e74c90006423719e5897216902a0ed16942
woollysammoth <amgenstudios@gmail.com>
2017-02-05 16:39:18 -0500

    Adding smoke

--- public/js/global.js
+++ public/js/global.js
@@ -25,6 +25,7 @@
             autoplay: false,
             events: {
                 'onReady': function(event){
+                    event.target.setVolume(0);
                     window.DeanTown.player = event.target;
                     window.DeanTown.videoReady = true;
                 },
