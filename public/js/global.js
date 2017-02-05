f44dbf66bdeb83290eee58ea23c770dfdbe7bd45
woollysammoth <amgenstudios@gmail.com>
2017-02-05 13:58:38 -0500

    Adding smoke

--- public/js/global.js
+++ public/js/global.js
@@ -1,26 +1,26 @@
-var canvas = document.getElementById('smoke')
-var ctx = canvas.getContext('2d')
-canvas.width = 1000
-canvas.height = 1000
+var canvas = document.getElementById('smoke');
+var ctx = canvas.getContext('2d');
+canvas.width = 1000;
+canvas.height = 1000;
 
-var smoke = smokemachine(ctx, [54, 16.8, 18.2])
+var smoke = smokemachine(ctx, [54, 16.8, 18.2]);
 
-smoke.start()
-smoke.addsmoke(500,500,10)
+smoke.start();
+smoke.addsmoke(500,500,10);
 
 setTimeout(function(){
 
-    smoke.stop()
+    smoke.stop();
 
-    smoke.addsmoke(600,500,100)
-    smoke.addsmoke(500,600,20)
+    smoke.addsmoke(600,500,100);
+    smoke.addsmoke(500,600,20);
 
     for(var i=0;i<10;i++){
-        smoke.step(10)
+        smoke.step(10);
     }
 
     setTimeout(function(){
-        smoke.start()
-    },1000)
+        smoke.start();
+    },1000);
 
-},1000)
+},1000);
